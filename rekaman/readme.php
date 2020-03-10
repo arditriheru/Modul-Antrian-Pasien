58c92186ddf51cbbe241416b3c98b3b4a85e81c6
Ardi Tri Heru <37565758+arditriheru@users.noreply.github.com>
2020-03-10 11:15:50 +0700

    versi1

---
+++ rekaman/readme.php
@@ -0,0 +1,9 @@
+<!--
+	Name        : Modul Antrian Rumah Sakit
+	Description : Sistem untuk antrian digital.
+	Date        : 2019
+	Developer   : Ardi Tri Heru
+	Phone/WA    : 0896-2967-1717
+	Email       : arditriheruh@gmail.com
+	Website     : https://www.arditriheru.com
+-->
